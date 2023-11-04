a0eb0b2c2b400c43540351707f4fc6fa73f55c30
khushi <khushichaudhary21@gnu.ac.in>
2023-11-04 20:01:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical11_21012011010/Person_name.kt
@@ -0,0 +1,11 @@
+package com.example.mad_practical11_21012011010
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Person_name : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_person_name)
+    }
+}
